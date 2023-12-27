87c10bcdbf3960274b4fccb8370e70c3cdabf4eb
Venkatesh <venkateshtata9@gmail.com>
2023-12-27 05:57:41 +0530

    updated quant on bert

--- quantize.py
+++ quantize.py
@@ -1,41 +1,47 @@
 import torch
 import time
-
+import collections
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer, BertModel
 import torch.quantization
 from torch.quantization import get_default_qconfig, QConfig
 import transformers
+from torch.ao.quantization import quantize_dynamic, default_dynamic_qconfig, float_qparams_weight_only_qconfig
+
 
 
 model = BertModel.from_pretrained('bert-base-uncased')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-model.eval()
+# torch.save(model.state_dict(), "normal.pth")
 
-# Function to apply dynamic quantization to specific layers - encoder.layer.3.output.LayerNorm.weight
-def quantize_attention_heads(model):
-    for name, module in model.named_modules():
-        # print("name: ", name)
-        # print("module: ", module)
-        # print("instance class: ", type(module))
-        # print("==========")
-        # Check if the module is part of the attention mechanism
-        if name=="encoder.layer.11.intermediate.dense" and isinstance(module, torch.nn.modules.linear.Linear):
-            # Quantize the module dynamically
-            torch.quantization.quantize_dynamic(module, dtype=torch.qint8, inplace=True)
+def collect_unique_module_types(model):
+    module_types = collections.defaultdict(int)
+    for module in model.modules():
+        module_types[type(module)] += 1
+    return module_types
 
-quantize_attention_heads(model)
+# Collect unique module types
+unique_module_types = collect_unique_module_types(model)
+
+# Print the unique module types
+print("Unique module types in the model:")
+for module_type, count in unique_module_types.items():
+    print(f"{module_type.__name__}: {count} instances")
+    
+
+model.qconfig = default_dynamic_qconfig
+
+# Special configuration for embedding layers
+for name, module in model.named_modules():
+    module.qconfig = float_qparams_weight_only_qconfig
+    torch.quantization.quantize_dynamic(module, dtype=torch.qint8, inplace=True)
 
 
+# torch.save(model.state_dict(), "quant.pth")
 
 text = ["Example text for quantized BERT."]
 encoded_input = tokenizer(text, return_tensors='pt')
 
-# with torch.no_grad():
-#     quantized_output = quantized_model(**encoded_input)
 
-
-# Call the function with the loaded model
-for name, param in model.named_parameters():
-    print(f"{name}: {param.dtype}\n")
+print(model)
